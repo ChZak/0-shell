27e45d4b9dbcbf2ef80e83e8911cb0188e93a396
chzak <zakaria.chaikhi.pro@gmail.com>
2023-11-25 11:43:48 +0100

    good

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,50 @@
 use std::env;
 use std::io::{self, Write};
-use std::path::Path;
-use std::process::Command;
-use signal_hook::{iterator::Signals, consts::SIGINT};
-use std::thread;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use colored::*;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
+fn green(text: &str) -> String {
+    format!("\x1b[32m{}\x1b[0m", text)
+}
 
 fn main() {
-    let is_terminated = Arc::new(AtomicBool::new(false));
-    let is_terminated_clone = Arc::clone(&is_terminated);
+    // Stock le répertoire courant(racine) y retourner avec la commande cd sans argument
+    let project_base = env::current_dir().unwrap(); 
 
-    thread::spawn(move || {
-        let mut signals = Signals::new(&[SIGINT]).unwrap();
-        for sig in signals.forever() {
-            if sig == SIGINT {
-                is_terminated_clone.store(true, Ordering::SeqCst);
-                println!("\nInterruption Ctrl+C détéctée. Appuez sur 'exit' pour quitter.");
-            }
-        }
-    });
     loop {
         let current_path = env::current_dir().unwrap().display().to_string();
-        print!("{} $ ", current_path);
+        print!("{} $ ", green(&current_path));
         io::stdout().flush().unwrap();
 
-        if is_terminated.load(Ordering::SeqCst) {
-            break;
-        }
-
         let mut input = String::new();
-        if let Err(_) = io::stdin().read_line(&mut input) {
-            if is_terminated.load(Ordering::SeqCst) {
-                break;
-            }
-            continue;
-        }
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap_or("");
-
-        match command {
+        match input {
             "exit" => break,
-            "cd" => {
-                let new_dir = parts.next().unwrap_or("/");
-                if let Err(e) = env::set_current_dir(Path::new(new_dir)) {
+            command if command.starts_with("cd") => {
+                let parts: Vec<&str> = command.split_whitespace().collect();
+                let new_dir = if parts.len() > 1 {
+                    PathBuf::from(parts[1])
+                } else {
+                    project_base.clone()
+                };
+
+                if let Err(e) = env::set_current_dir(new_dir) {
                     eprintln!("cd: {}", e);
                 }
             },
-            "echo" => {
-                let echo_output = parts.collect::<Vec<&str>>().join(" ");
-                println!("{}", echo_output);
-            },
-            "pwd" => {
-                let output = Command::new("pwd").output().unwrap();
-                println!("{}", String::from_utf8_lossy(&output.stdout));
-            },
-            "ls" => {
-                let args = parts.collect::<Vec<&str>>();
-                let mut command = Command::new("ls");
-                command.args(args);
-                let output = command.output().unwrap();
+            command => {
+                let mut child = Command::new("zsh")
+                    .arg("-c")
+                    .arg(command)
+                    .stdout(Stdio::inherit())
+                    .stderr(Stdio::inherit())
+                    .spawn()
+                    .expect("failed to execute process");
 
-                println!("{}", String::from_utf8_lossy(&output.stdout));
-            },
-            "cat" => {
-                let args = parts.collect::<Vec<&str>>();
-                let mut command = Command::new("cat");
-                command.args(args);
-                let output = command.output().unwrap();
-
-                println!("{}", String::from_utf8_lossy(&output.stdout));
-            },
-            "cp" | "mv" | "rm" | "mkdir" => {
-                let args = parts.collect::<Vec<&str>>();
-                let output = Command::new(command)
-                    .args(args)
-                    .output()
-                    .unwrap();
-
-                if !output.stderr.is_empty() {
-                    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-                }
+                let _ = child.wait().expect("failed to wait on child");
             }
-            _ => println!("Commande '{}' non trouvée", command.red()),
         }
     }
 }
